0c5545be1a558da9d22af374f8ef6eabc4326bff
Anqap <arifcahyogd7@gmail.com>
2026-05-09 13:12:58 +0700

    update preprocessing

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -2,8 +2,6 @@ import pandas as pd
 import mlflow
 import mlflow.sklearn
 from sklearn.ensemble import RandomForestRegressor
-import shutil
-import os
 
 def train():
     print("Memuat data preprocessing...")
@@ -12,19 +10,14 @@ def train():
     X_train = train_df.drop('median_house_value', axis=1)
     y_train = train_df['median_house_value']
 
+    # Autolog akan otomatis menyimpan model ke folder 'mlruns/0/<RUN_ID>/artifacts/model'
     mlflow.sklearn.autolog()
     
     with mlflow.start_run():
         print("Melatih model...")
         model = RandomForestRegressor(n_estimators=50, random_state=42)
         model.fit(X_train, y_train)
-
-        print("Menyimpan artefak model untuk Docker...")
-        if os.path.exists("saved_model"):
-            shutil.rmtree("saved_model")
-            
-        mlflow.sklearn.save_model(model, "saved_model")
-        print("Model berhasil disimpan di folder 'saved_model'.")
+        print("Selesai! Model otomatis tersimpan di dalam folder mlruns.")
 
 if __name__ == "__main__":
     train()
